9d1b9007f7028df7f2ac17eaa95bbc47caceb440
SaltyChickenLeg <merijnjong1@gmail.com>
2023-10-11 18:19:08 +0200

    changed memset function

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 15:11:08 by mjong             #+#    #+#             */
-/*   Updated: 2023/10/11 14:59:38 by mjong            ###   ########.fr       */
+/*   Updated: 2023/10/11 18:17:43 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,29 +30,27 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t			i;
 	unsigned char	*str;
 	unsigned char	ch;
 
-	i = 0;
 	str = (unsigned char *)s;
 	ch = (unsigned char)c;
-	while (i < n)
+	while (*str < n)
 	{
-		str[i] = ch;
-		i++;
+		*str = ch;
+		str++;
 	}
 	return (str);
 }
 
-// int	main(void)
-// {
-//     char buffer[10] = "BBBBBBBBBB";
+int	main(void)
+{
+    char buffer[10] = "BBBBBBBBBB";
 
-//     char *result1 = ft_memset(buffer, 'A', 5);
-// 	char *result2 = memset(buffer, 'A', 5);
+    char *result1 = ft_memset(buffer, 'A', 5);
+	char *result2 = memset(buffer, 'A', 5);
 
-// 	printf("%s\n", result1);
-// 	printf("%s\n", result2);
-// 	return (0);
-// }
+	printf("%s\n", result1);
+	printf("%s\n", result2);
+	return (0);
+}
