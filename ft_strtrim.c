32219b76b0cab98e1efb9faf33ef80c234e26673
SaltyChickenLeg <merijnjong1@gmail.com>
2023-10-18 16:12:23 +0200

    added ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,20 +6,52 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 13:44:16 by mjong             #+#    #+#             */
-/*   Updated: 2023/10/18 12:55:14 by mjong            ###   ########.fr       */
+/*   Updated: 2023/10/18 16:07:36 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <unistd.h>
-// #include <stdio.h>
-// #include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
 
-// char	*ft_strtrim(char const *s1, char const *set)
-// {
+int	ft_iterate(char strc, const char *set)
+{
+	while (*set != '\0')
+	{
+		if (strc == *set)
+			return (1);
+		else
+			set++;
+	}
+	return (0);
+}
 
-// }
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	size_t	len;
+	char	*str;
+
+	start = 0;
+	end = ft_strlen(s1);
+	while (ft_iterate(s1[start], set) == 1)
+	{
+		start++;
+	}
+	while (ft_iterate(s1[end - 1], set) == 1)
+	{
+		end--;
+	}
+	len = end - start;
+	str = ft_substr(s1, start, len);
+	return (str);
+}
 
 // int	main(void)
 // {
-	
+// 	char	*trimmedstring = ft_strtrim("unununTRIMununun", "un");
+
+// 	printf("%s", trimmedstring);
 // }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 11:09:24 by mjong             #+#    #+#             */
-/*   Updated: 2023/10/18 13:25:50 by mjong            ###   ########.fr       */
+/*   Updated: 2023/10/18 16:08:22 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ int		ft_strlen(const char *str);
 int		ft_strncmp(char *s1, char *s2, int n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
