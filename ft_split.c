fb08b519f43b5c8ec7d166842caf0b41fadc355d
SaltyChickenLeg <merijnjong1@gmail.com>
2023-10-19 15:53:44 +0200

    made ft_split function

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 16:31:05 by mjong             #+#    #+#             */
-/*   Updated: 2023/10/18 17:28:25 by mjong            ###   ########.fr       */
+/*   Updated: 2023/10/19 15:53:17 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,101 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "libft.h"
 
-static int	ft_wordcounter(char const *s, char c)
+static int	ft_stringcounter(char const *s, char c)
 {
-	int	spaces;
+	int	strings;
+	int	sign;
 
-	spaces = 0;
+	strings = 0;
+	sign = 0;
 	while (*s != '\0')
 	{
-		while (*s == c)
+		if (*s != c && sign == 0)
 		{
-			spaces++;
-			s++;
+			strings++;
+			sign = 1;
+		}
+		if (*s == c)
+		{
+			sign = 0;
 		}
 		s++;
 	}
-	if (*s == '\0')
-		spaces++;
-	return (spaces);
+	return (strings);
+}
+
+static int	ft_length(const char *s, char c, int i)
+{
+	int	j;
+
+	j = 0;
+	while (s[i] != '\0' && s[i] != c)
+	{
+		i++;
+		j++;
+	}
+	return (j);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	size_t	strings;
 	char	**str;
+	int		strings;
+	int		i;
+	int		j;
 
-	strings = ft_wordcounter(s, c);
-	str = malloc(strings);
+	strings = ft_stringcounter(s, c);
+	str = malloc((strings + 1) * sizeof(char *));
+	i = 0;
+	j = 0;
+	if (!str || !s)
+		return (str);
+	while (i < strings)
+	{
+		while (s[j] == c)
+		{
+			j++;
+		}
+		str[i] = ft_substr(s, j, ft_length(s, c, j));
+		if (str[i] == NULL)
+			free(str[i]);
+		j += ft_length(s, c, j);
+		i++;
+	}
+	str[i] = NULL;
 	return (str);
 }
 
-int	main(void)
-{
-	ft_split("Hello world how are you", ' ');
-	return (0);
-}
+// int	main(void)
+// {
+// 	char	input_string[] = "Hello,world,how,are,you,,";
+// 	char	delimiter = ',';
+
+// 	char **result = ft_split(input_string, delimiter);
+
+// 	if (result)
+// 	{
+// 		int	i;
+
+// 		i = 0;
+// 		while (result[i] != NULL)
+// 		{
+// 			printf("Substring %d: %s\n", i, result[i]);
+// 			i++;
+// 		}
+// 		i = 0;
+// 		while (result[i] != NULL) {
+// 			free(result[i]);
+// 			i++;
+// 		}
+// 		free(result);
+// 	}
+// 	else
+// 	{
+// 		printf("Memory allocation failed or input string is NULL.\n");
+// 	}
+
+// 	return (0);
+// }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 11:09:24 by mjong             #+#    #+#             */
-/*   Updated: 2023/10/18 16:08:22 by mjong            ###   ########.fr       */
+/*   Updated: 2023/10/19 13:53:18 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
