16d1948bc570edea282cfe69c82600836996db5e
SaltyChickenLeg <merijnjong1@gmail.com>
2023-10-23 15:05:37 +0200

    made ft_itoa.c function

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 17:25:14 by mjong             #+#    #+#             */
-/*   Updated: 2023/10/19 17:34:59 by mjong            ###   ########.fr       */
+/*   Updated: 2023/10/23 15:03:17 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,53 @@
 #include <stdlib.h>
 #include "libft.h"
 
+static int	ft_intlencount(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n < 0)
+		len++;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
 char	*ft_itoa(int n)
 {
+	int		len;
+	long	nb;
 	char	*str;
 
-	str = n;
+	len = ft_intlencount(n);
+	nb = n;
+	str = (char *)malloc(len + 1);
 	if (!str)
 		return (NULL);
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	if (nb == 0)
+		str[0] = '0';
+	str[len--] = '\0';
+	while (nb)
+	{
+		str[len] = nb % 10 + '0';
+		nb /= 10;
+		len--;
+	}
 	return (str);
 }
 
-int	main(void)
-{
-	char *result = ft_itoa(10400);
+// int	main(void)
+// {
+// 	char *result = ft_itoa(-2147483648);
 
-	printf("%s", result);
-	return (0);
-}
+// 	printf("%s", result);
+// 	return (0);
+// }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 11:09:24 by mjong             #+#    #+#             */
-/*   Updated: 2023/10/19 13:53:18 by mjong            ###   ########.fr       */
+/*   Updated: 2023/10/23 15:04:28 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+char	*ft_itoa(int n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
